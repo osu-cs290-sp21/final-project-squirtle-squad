0ccbc4a113daae6d534bf2e1cda35e2a7ec4c4f6
Thomas B <tpburg8@gmail.com>
2021-05-29 15:58:43 -0700

    No longer using csv, now json
    Everything is much easier to read when the json is saved to a file and now instead of converting it every time, we save the json itself

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var pokemonArray = require('./resources/pokemon.json');
 
 port = 3000;
-var pokemonArray;
 
 app.use(express.static('public'));
 
@@ -18,49 +18,6 @@ app.get('/pokemon/:id', function (req, res, next) {
     
 })
 
-//This function turns the csv storing our data for stats into a json
-function csvJSON(csv){
-
-    var lines=csv.split("\n");
-  
-    var result = [];
-  
-    var headers=lines[0].split(",");
-
-    //Get our sprites
-    var rawSpritesTable = fs.readFileSync("resources/spriteSheets_Links").toString().split("\n");
-  
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        //The abilities array is redundant, so we remove it since the commas inside make it harder to use
-        var charactersToRemove = lines[i].indexOf("\"", 1)+1;
-        var data = lines[i].slice(charactersToRemove);
-        var currentline=data.split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-        if(i < rawSpritesTable.length+1) {
-            var sprite = rawSpritesTable[i-1].slice(0,-1);
-            obj.sprite = sprite.split(" ")[2];
-        }
-        
-        result.push(obj);
-  
-    }
-    
-    return result; //JavaScript object
-    // return JSON.stringify(result); //JSON
-}
-
-function setup() {
-    var csv = fs.readFileSync("resources/pokemon.csv").toString();
-    pokemonArray = csvJSON(csv);
-    console.log(pokemonArray[0]);
-}
-
-setup();
 
 app.listen(port, function () {
     console.log("Server has begun listening on port " + port);

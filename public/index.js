bd253f80b2e3ae85dd84aa8fcc679f2b1bbc756a
Thomas B <tpburg8@gmail.com>
2021-06-08 10:56:26 -0700

    Search now maintains the correct position of custom pokemon list
    Wasn't that hard, just always insert the container of pokemon into the second position if it is in position 2

--- public/index.js
+++ public/index.js
@@ -1,81 +1,3 @@
-
-var pokeStats = document.querySelectorAll(".stat")
-
-var pokeAttackUser = pokeStats[1].textContent
-var userAttack = pokeAttackUser.replace('ATK:', '')
-
-var pokeDefenseUser = pokeStats[2].textContent
-var userDefense = pokeDefenseUser.replace('DEF:', '')
-
-var pokeAttackOppo = pokeStats[7].textContent
-var oppoAttack = pokeAttackOppo.replace('ATK:', '')
-
-var pokeDefenseOppo = pokeStats[8].textContent
-var oppoDefense = pokeDefenseOppo.replace('DEF:', '')
-
-var attackButton = document.querySelectorAll('.move')
-if(window.location.href == "http://localhost:3000/battle"){
-  attackButton[0].addEventListener('click', function(){attack(0, attackButton[0])})
-  attackButton[1].addEventListener('click', function(){attack(0, attackButton[1])})
-  attackButton[2].addEventListener('click', function(){attack(0, attackButton[2])})
-  attackButton[3].addEventListener('click', function(){attack(0, attackButton[3])})
-}
-
-function attack(whichMon, move){
-  var damage = 0
-  var critical = 1
-  var pokeHealthOppo = document.getElementById('opponenthealth').textContent
-  var oppoHealthA = pokeHealthOppo.split('/')[0]
-  var oppoHealthB = pokeHealthOppo.split('/')[1]
-  var pokeHealthUser = document.getElementById('userhealth').textContent
-  var userHealthA = pokeHealthUser.split('/')[0]
-  var userHealthB = pokeHealthUser.split('/')[1]
-  var moveAccuracyU = move.querySelector(".accuracy")
-  var moveAccuracy = moveAccuracyU.textContent.replace('Accuracy:', '')
-  var movePowerU = move.querySelector(".power")
-  var movePower = movePowerU.textContent.replace('Power:', '')
-  var movePPU = move.querySelector(".pp")
-  var movePP = movePPU.textContent.replace('PP:', '')
-
-  if (whichMon == 0){
-    if ((moveAccuracy * 100) >= Math.floor(Math.random() *100)){
-      if(Math.floor(Math.random()*24) == 1){
-        critical = 1.5
-      }
-      damage = ((((userAttack * movePower)/oppoDefense)/50)+2) * critical
-      movePP.textContent = movePP - 1
-    }
-    var newHealth = Math.round(oppoHealthA - damage)
-    document.getElementById('opponenthealth').innerHTML = newHealth + "/" + oppoHealthB
-  }
-  if (whichMon == 1){
-    if ((moveAccuracy * 100) >= Math.floor(Math.random() *100)){
-      if(Math.floor(Math.random()*24) == 1){
-        critical = 1.5
-      }
-      damage = ((((oppoAttack * movePower)/userDefense)/50)+2) * critical
-      movePP.textContent = movePP - 1
-    }
-    var newHealth = Math.round(userHealthA - damage)
-    document.getElementById('userhealth').innerHTML = newHealth + "/" + userHealthB
-  }
-
-  if (0 >= newHealth ){
-    if (whichMon == 0){
-      alert("Your Opponent's Pokemon has fainted. You Win! (Restart page to retry)")
-    }
-    else{
-      alert("Your Pokemon has fainted. You Lost! (Restart page to retry)")
-    }
-  }
-
-  if (whichMon == 0){
-    var rand = Math.floor(Math.random() * 4)
-    attack(1,attackButton[rand+4])
-  }
-}
-
-
 var pokemonContainer = document.getElementsByClassName('pokemon-container')
 var savedPokemon = []
 var allPokemon = document.getElementsByClassName('pokemon flexItem')
@@ -207,17 +129,13 @@ function searchByClicking() {
     if(!match) {
       searchCard.parentNode.parentNode.remove();
     }
-    // var containerOfPokemon = document.getElementsByClassName('pokemon-container')
-    // var test = containerOfPokemon[0].getElementsByClassName('flexItem')
-    // var customPokemonCard = containerOfPokemon[0].getElementsByClassName('customPokemonViewer')
-    // if(test[0]==customPokemonCard[0]){
-    //   console.log("Hello")
-    //   var temp = test[0]
-    //   test[0] = test[1]
-    //   test[1] = temp
-    // }
-    // console.log("pokemon container: ", test[0], test[1])
-    // console.log(customPokemonCard[0])
+    var containerOfPokemon = document.getElementsByClassName('pokemon-container')[0];
+    var custPokeBox = document.getElementsByClassName("customPokemonViewer")[0]
+    if(containerOfPokemon.children[0] == custPokeBox) {
+        if(containerOfPokemon.childElementCount > 1) {
+            containerOfPokemon.insertBefore(containerOfPokemon.children[1], custPokeBox)
+        }
+    }
   })
 }
 
